f90716facfca7b48ca3be448a81a227ff9ffcc0a
Petemab <peteben18@hotmail.com>
2018-06-06 15:47:23 +0100

    solved error with creating new

--- db/seeds.js
+++ db/seeds.js
@@ -9,14 +9,14 @@ mongoose.connect(dbURI, (err, db) => {
 
   Place
     .create([{
-      name: "London Bridge",
+      name: 'London Bridge',
       address: 'London SE1 9DD',
       location: { lat: 51.507879, lng: -0.087732 },
       book: 'Oliver Twist',
-      description: "Several bridges named London Bridge have spanned the River Thames between the City of London and Southwark, in central London. The current crossing, which opened to traffic in 1973, is a box girder bridge built from concrete and steel. It replaced a 19th-century stone-arched bridge, which in turn superseded a 600-year-old stone-built medieval structure. This was preceded by a succession of timber bridges, the first of which was built by the Roman founders of London.",
+      description: 'Several bridges named London Bridge have spanned the River Thames between the City of London and Southwark, in central London. The current crossing, which opened to traffic in 1973, is a box girder bridge built from concrete and steel. It replaced a 19th-century stone-arched bridge, which in turn superseded a 600-year-old stone-built medieval structure. This was preceded by a succession of timber bridges, the first of which was built by the Roman founders of London.',
       image: 'https://en.wikipedia.org/wiki/London_Bridge#/media/File:London_Bridge_Illuminated.jpg'
     }
-  ])
+    ])
     .then(places => console.log(`${places.length} places created!`))
     .catch(err => console.log(err))
     .finally(() => mongoose.connection.close());
