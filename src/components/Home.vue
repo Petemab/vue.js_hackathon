a1546845efdb2ae4ea046ded9d655f08def7977c
Petemab <peteben18@hotmail.com>
2018-06-07 11:26:02 +0100

    stlyed homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,29 +2,25 @@
   <section class="section homeSection">
     <div class="container">
       <div class="columns">
-        <div class="column is-half">
+        <div class="column is-three-fifths is-offset-one-fifth">
           <div class="homeWords has-text-centered">
             <h1 class="titleHome">Britain By The Book</h1>
             <hr/>
             <h2 class="subtitleHome">Explore the UK city by city and page by page</h2>
-            <p class="homeP">Click on Oliver Twist below to begin</p>
+            <p class="homeP">Click on Charles Dickens to begin</p>
             <hr/>
           </div>
           <div class="columns">
             <div class="column is-one-third">
             </div>
-            <router-link tag="div" to="/places" class="twistPic column is-one-third">
+            <router-link tag="div" to="/places" class="homePic column is-one-third">
             </router-link>>
             <div class="column is-one-third">
             </div>
           </div>
 
         </div>
-        <div class="column is-half">
-          <div class="homePic">
 
-          </div>
-        </div>
       </div>
     </div>
   </section>
