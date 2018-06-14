dae7c4b1fb03cb4088bca69621b499bde987225c
Stephanie Ye <stephanie.ye@gmail.com>
2018-06-14 13:08:00 +0100

    heroku

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router';
 
 import './assets/scss/style.scss';
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 import Home from './components/Home';
