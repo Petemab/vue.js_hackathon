3e1d74104ada4181759daa034af23c86e7569402
Stephanie Ye <stephanie.ye@gmail.com>
2018-06-14 13:40:38 +0100

    heroku

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/litmap-${env}`;
+// const dbURI = process.env.MONGODB_URI || `mongodb://localhost/litmap-${env}`;
+const dbURI = 'https://britainbythebook.herokuapp.com';
 const secret = process.env.SECRET || 'a gigantic hound!';
 
 module.exports = { port, dbURI, secret, env };
